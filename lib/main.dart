9a4fe7472da7d0b33e536033ba9a67a9a2aa4252
Sizan <mahatosizan@gmail.com>
2024-06-25 20:25:06 +0545

    firebase update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
@@ -10,9 +9,7 @@ Future<void> main() async {
   runApp(const App());
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform).then(
-      (FirebaseApp value) => Get.put(AuthenticationRepository())
-  );
+    options: DefaultFirebaseOptions.currentPlatform);
 
 
 }
