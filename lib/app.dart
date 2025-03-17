476cd66560e1a9c708ee6bb6b06fb512c103353c
Sizan <140323399+Siz-An@users.noreply.github.com>
2025-03-17 21:29:40 +0545

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,6 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -23,5 +22,4 @@ class App extends StatelessWidget {
       ),
     );
   }
-
 }
